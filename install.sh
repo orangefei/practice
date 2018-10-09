df140518195852ada4f3e88aecca34fc82515f0e
orangefei <121738751@qq.com>
2018-10-09 17:17:04 +0800

    delete

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-touch /root/zengfanzhu/djangoInstallInfo.txt
-/usr/bin/python /root/zengfanzhu/Django-1.6.11/setup.py install
-find /{bin,lib,lib64,sbin,usr} -cnewer /root/zengfanzhu/djangoInstallInfo.txt -type f -print >> /root/zengfanzhu/djangoInstallInfo.txt
-mv /root/zengfanzhu/djangoInstallInfo.txt /root/zengfanzhu/djangoInstallInfo.`date "+%Y-%m-%d-%H-%M-%S"`.txt
-if [ -f "/root/zengfanzhu/djangoInstallInfo.txt" ];then
-  rm "/root/zengfanzhu/djangoInstallInfo.txt"
-fi
